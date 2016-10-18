bea1b2c56d9da8f33b2e9e598b162b62b19cb9a0
Leah Dao <leahgiahan@yahoo.com>
2016-10-18 15:11:42 -0700

    added attempted exercises

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -4,15 +4,20 @@
 
 # Install devtools package: allows installations from GitHub
 install.packages('devtools')
+install.packages('dplyr')
 
 # Install "fueleconomy" package from GitHub
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
+library(fueleconomy)
+library(dplyr)
 
-
+View(vehicles)
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+acuras <- filter(vehicles, make == 'Acura', year == 2015)
+best.acura <- filter(acuras, hwy == max(hwy))
+best.model <- select(best.acura, model)
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
 
